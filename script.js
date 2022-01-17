71ec898967804e18685db0c5f1b1ce1f3c95e574
federico-palma <fedepal9@gmail.com>
2022-01-17 12:05:28 +0100

    Added event listeners for hover

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 let gridContainer = document.getElementById('grid-container');
-let gridDefinition = 16
 
-
+let gridDefinition = 64
 gridContainer.style.gridTemplateRows = `repeat(${gridDefinition}, 1fr)`
 gridContainer.style.gridTemplateColumns = `repeat(${gridDefinition}, 1fr)`
 
 for (let i = 0; i < (gridDefinition * gridDefinition); i++) {
     let newCell = document.createElement('div');
     newCell.classList.add('grid-cell');
+    newCell.addEventListener('mouseover', changeColor);
     gridContainer.appendChild(newCell);
-    console.log(newCell);
 };
+
+function changeColor() {
+    event.target.style.backgroundColor = 'black';
+}
 
