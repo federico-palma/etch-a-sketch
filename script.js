3ed65e3f4a31adac854cccba5c5fe183ab49eb3c
federico-palma <fedepal9@gmail.com>
2022-01-18 10:07:57 +0100

    Created function for grid creation

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
-// Grid creation and setting of event listener.
+// Grid creation.
 let gridContainer = document.getElementById('grid-container');
 
 let gridDefinition = 64
-gridContainer.style.gridTemplateRows = `repeat(${gridDefinition}, 1fr)`
-gridContainer.style.gridTemplateColumns = `repeat(${gridDefinition}, 1fr)`
-
-for (let i = 0; i < (gridDefinition * gridDefinition); i++) {
-    let newCell = document.createElement('div');
-    newCell.classList.add('grid-cell');
-    newCell.addEventListener('mouseover', changeColor);
-    gridContainer.appendChild(newCell);
-};
 let gridCells = document.getElementsByClassName('grid-cell')
 
+// Function sets the grid according to the Grid Definition chosen and adds event listener to grid cells.
+function setGrid() {
+    gridContainer.style.gridTemplateRows = `repeat(${gridDefinition}, 1fr)`
+    gridContainer.style.gridTemplateColumns = `repeat(${gridDefinition}, 1fr)`
+
+    for (let i = 0; i < (gridDefinition * gridDefinition); i++) {
+        let newCell = document.createElement('div');
+        newCell.classList.add('grid-cell');
+        newCell.addEventListener('mouseover', changeColor);
+        gridContainer.appendChild(newCell);
+    };
+};
+
+setGrid();
 function changeColor() {
     this.style.backgroundColor = 'black';
-}
+};
 
-// Clear button
-let clearBtn = document.getElementById('btn-clear');
-clearBtn.addEventListener('click', () => {
+// Clear function and button.
+function clearGrid() {
     for (let i = 0; i < gridCells.length; i++) {
         gridCells[i].style.backgroundColor = 'white';
     }
-});
+}
+
+let clearBtn = document.getElementById('btn-clear');
+clearBtn.addEventListener('click', clearGrid);
